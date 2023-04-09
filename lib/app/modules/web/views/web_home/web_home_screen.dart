6b832f9321e07c65012f66d3882e590d800ecf64
cookkiees <124230760+cookkiees@users.noreply.github.com>
2023-04-10 01:19:25 +0700

    features web

--- lib/app/modules/web/views/web_home/web_home_screen.dart
+++ lib/app/modules/web/views/web_home/web_home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
-
+import '../../../../utils/my_colors.dart';
 import '../../web_controller.dart';
 
 class WebHomeScreen extends GetView<WebController> {
@@ -8,6 +9,40 @@ class WebHomeScreen extends GetView<WebController> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: MyColors.backgrundColor,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 32, right: 32),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 450,
+                  width: 800,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF609EAF),
+                    borderRadius: BorderRadius.circular(48),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Image.asset(
+                          "assets/images/eric-esma.png",
+                          fit: BoxFit.fill,
+                          height: 450,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
