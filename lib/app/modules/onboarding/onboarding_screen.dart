c5d203d2dc2b4b4ea2a795fcdd4fc5f737694ac5
cookkiees <124230760+cookkiees@users.noreply.github.com>
2023-04-09 18:01:47 +0700

    add size config

--- lib/app/modules/onboarding/onboarding_screen.dart
+++ lib/app/modules/onboarding/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_music_responsive_app/app/config/size_config.dart';
 import 'package:get/get.dart';
 import 'onboarding_controllers.dart';
 
@@ -7,6 +8,8 @@ class OnboardingScreen extends GetView<OnboardingController> {
 
   @override
   Widget build(BuildContext context) {
+    // To use size config
+    SizeConfig().init(context);
     return Container();
   }
 }
